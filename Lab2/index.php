408dac9b16bb1f7639c5a6d953efe993cae2deff
Kyle <josiekausl266@gmail.com>
2021-09-19 20:18:21 +0800

    I fucked

--- Lab2/index.php
+++ Lab2/index.php
@@ -1,26 +1,30 @@
-<?php 
+<?php
+    // submit button дарагдан хийгдэх үед 
     if(isset($_POST['submit'])){
         $val = $_POST['value'] ? $_POST['value'] : null;
         $result = 0;
+        // POST request - ээс орж ирсэн хариуг шалгаад хоосон тохиолдолд null 
+        // mesaurmenta_type нь 
         $measurment_type = isset($_POST['convert_types']) ? $_POST['convert_types'] : null;
+        // Хувиргах тоо нь тоо биш эсвэл хоосон тохиолдолд дэлгэцэнд 0 гэсэн тоог харуулана 
         if(!$val || !is_numeric($val) ) {
             $result = 0;
             $measurment_type = '';
         }
         switch($measurment_type){
-            case 'инч':
+            case 'см':
                 $result = $val * 2.54; 
                 break;
-            case 'бээр':
+            case 'км':
                 $result = $val / 1094;
                 break;
-            case 'паунд':
+            case 'грам':
                 $result = $val * 454;
                 break;
-            case 'морины хүч': 
+            case 'ватт': 
                 $result = $val * 746;
                 break;
-            case 'баррел': 
+            case 'литр': 
                 $result = $val * 159;
                 break;
             default:
@@ -37,13 +41,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap san -->
+    <!-- Bootstrap сан -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel = 'stylesheet' href="./style.css" />
     <title>Form</title>
 </head>
 <body>
     <div class = "container"> 
+        <!-- action нь хуудсын өгөгдлийг хаашаа дамжуулахыг заах бөгөөд POST method ашиглан өгөгдлийг дамжуулна -->
         <form action = "./index.php" method="post">
            <div class = "container_top"> <p>Утга хөрвүүлэх</p> </div>
            <div class = "container_middle"> 
@@ -56,18 +61,21 @@
                         <label for="text">Хувиргах төрөл</label>
                         <select name = "convert_types" class="form-select" aria-label="Default select example">
                             <option>-сонго-</option>
-                            <option value="инч">Инч-СМ</option>
-                            <option value="бээр">Бээр-КМ</option>
-                            <option value="паунд">Паунд-ГРАМ</option>
-                            <option value="морины хүч">Морины хүч-Ватт</option>
-                            <option value = "баррел">Баррелийг-литр</option>
+                            <option value="см">Инч-СМ</option>
+                            <option value="км">Бээр-КМ</option>
+                            <option value="грам">Паунд-ГРАМ</option>
+                            <option value="ватт">Морины хүч-Ватт</option>
+                            <option value = "литр">Баррел-литр</option>
                         </select>
                     </div>
                    </div> 
                    <div class = "row">
                    <div class = "col-sm">
                     <label for = "text"> <span> Нэгжийн утга </span></label>
-                    <input type="text" name = "output" class="form-control" id = "disabledInput" value = "<?php echo isset($result) ? $result : ''; echo " "; echo $measurment_type ?>"disabled>
+                    <!-- Submit товч дарсны дараа өгөгдлийг боловсруулан гаргана -->
+                    <!-- Эхэлж өгөгдлийг байгаа эсэхийг isset function ашиглан шалгах ба өгөгдөл байхгүй
+                     тохиолдолд хоосон string хэвлэнэ, үнэн бол үр дүн (result) - ийг хэвлэнэ ✨   -->
+                    <input type="text" name = "output" class="form-control" id = "disabledInput" value = "<?php echo isset($result) ? $result : ''; echo " "; echo isset($measurment_type) ? $measurment_type : " "; ?>"disabled>
                    </div>
                </div>
            </div>
