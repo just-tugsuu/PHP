393d661b293a3760aac814f8fbb965286452f1c0
Mizu <josiekausl266@gmail.com>
2021-10-28 17:00:24 +0800

    refactor: yaml file усгагдсан

--- Lab5/index.php
+++ Lab5/index.php
@@ -1,17 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Hind:wght@400;500;600;700&display=swap" rel="stylesheet">
     <title>Хайлт</title>
 </head>
-<body>
 
-   <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" class="search-bar" method = "POST">
-   
-    </div>
-</form>
+<body>
+    <!-- action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" class="search-bar" method = "POST" -->
+    <!-- <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="Petname">
+            <label class="form-check-label" for="inlineRadio1">Pet name</label>
+        </div>
+        <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="Owner">
+            <label class="form-check-label" for="inlineRadio2">Owner</label>
+        </div> -->
+    <form class="container">
+     
+        <div class="search-bar">
+            
+        </div>
+        </div>
+    </form>
 </body>
+
 </html>
